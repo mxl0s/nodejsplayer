47c0b4509af83d689e087c3f33bfdd333a629a65
Milos <contact@milosv.co>
2023-05-22 19:38:40 +0200

    added shuffle, folder support, more animations, bulk download

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let songs = [];
 
 window.addEventListener('DOMContentLoaded', () => {
-  AOS.init(); // initialize AOS
   fetch('/songs')
     .then(response => response.json())
     .then(data => {
       songs = data;
       renderSongs(songs);
     });
+  AOS.init();
 });
 
 function renderSongs(songs) {
@@ -16,42 +16,58 @@ function renderSongs(songs) {
 
   songs.forEach(song => {
     const listItem = document.createElement('li');
-    listItem.setAttribute('data-aos', 'fade-up'); // add AOS attribute
+
+    const selectBox = document.createElement('input');
+    selectBox.type = 'checkbox';
+    selectBox.className = 'song-checkbox';
+    listItem.appendChild(selectBox);
 
     const songTitle = document.createElement('span');
     songTitle.textContent = song.title;
     listItem.appendChild(songTitle);
 
     listItem.addEventListener('click', () => {
-      loadSong(song.file);
+      if (!selectBox.checked) {
+        loadSong(song.file);
+      }
     });
 
     const downloadLink = document.createElement('a');
     downloadLink.href = song.file;
-    downloadLink.download = song.title; // This will make it download
+    downloadLink.download = song.title;
     downloadLink.innerHTML = '<i class="fas fa-download"></i>';
     downloadLink.addEventListener('click', (event) => {
-      event.stopPropagation(); // prevent the song from playing when the download button is clicked
+      event.stopPropagation();
     });
     listItem.appendChild(downloadLink);
 
     songList.appendChild(listItem);
   });
+
+  const checkboxes = document.querySelectorAll('.song-checkbox');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', toggleDownloadButton);
+  });
 }
 
 function loadSong(file) {
   const audioPlayer = document.getElementById('audioPlayer');
-  const allSongs = document.querySelectorAll('#songList li');
-  allSongs.forEach(song => song.classList.remove('playing'));
+  const playerControls = document.getElementById('playerControls');
   audioPlayer.src = file;
+  playerControls.classList.remove('hidden');
   audioPlayer.play();
-
-  const currentSong = [...allSongs].find(song => song.querySelector('a').href === location.origin + file);
-  if (currentSong) currentSong.classList.add('playing');
 }
 
-function searchSong() {
-  const searchTerm = document.querySelector('.search input').value.toLowerCase();
-  const filteredSongs = songs.filter(song => song.title.toLowerCase().includes(searchTerm));
-  renderSongs(filteredSongs);
+function toggleDownloadButton() {
+  const checkboxes = document.querySelectorAll('.song-checkbox:checked');
+  const downloadButton = document.getElementById('downloadBtn');
+  downloadButton.style.display = checkboxes.length > 0 ? 'block' : 'none';
 }
+
+document.getElementById('downloadBtn').addEventListener('click', () => {
+  const checkboxes = document.querySelectorAll('.song-checkbox:checked');
+  checkboxes.forEach(checkbox => {
+    const downloadLink = checkbox.parentElement.querySelector('a');
+    downloadLink.click();
+  });
+});
