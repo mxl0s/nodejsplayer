5fa047f224ae4fe4448fea1395ed751f93c8d02b
Milos <contact@milosv.co>
2023-05-22 19:38:21 +0200

    added shuffle, folder support, more animations, bulk download

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import musicMetadata from 'music-metadata';
 import recursive from 'recursive-readdir';
+import path from 'path';
 
 const app = express();
 const PORT = 1486;
@@ -13,25 +14,39 @@ app.get('/', (req, res) => {
 });
 
 app.get('/songs', async (req, res) => {
-  const mp3Files = [];
+  const fileMap = new Map();
   const files = await recursive(mp3Folder, ['!*.mp3']);
 
   for (let file of files) {
+    const dir = path.dirname(file);
     const metadata = await musicMetadata.parseFile(file);
     let title = metadata.common.title;
     if (!title) {
-      title = file.split('/').pop(); 
+      title = path.basename(file); 
     }
-    mp3Files.push({
+    const song = {
       title: title,
       artist: metadata.common.artist,
       file: file.replace(mp3Folder, ''),
+    };
+    if (!fileMap.has(dir)) {
+      fileMap.set(dir, []);
+    }
+    fileMap.get(dir).push(song);
+  }
+
+  const directoryList = [];
+  for (let [dir, songs] of fileMap.entries()) {
+    songs.sort((a, b) => a.title.localeCompare(b.title));
+    directoryList.push({
+      name: path.basename(dir),
+      songs: songs
     });
   }
 
-  mp3Files.sort((a, b) => a.title.localeCompare(b.title));
+  directoryList.sort((a, b) => a.name.localeCompare(b.name));
 
-  res.json(mp3Files);
+  res.json(directoryList);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
