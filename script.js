1b1e8e63f14cff6f87058337694274610c14568d
Milos <contact@milosv.co>
2023-05-27 01:09:39 +0200

    added renaming whatsapp files to Artist Name - Song name from metadata

--- script.js
+++ script.js
@@ -71,3 +71,63 @@ document.getElementById('downloadBtn').addEventListener('click', () => {
     downloadLink.click();
   });
 });
+
+// New function to format song titles starting with "AUD-"
+function formatSongTitle(song) {
+  if (song.title.startsWith("AUD-")) {
+    const parts = song.title.split(" - ");
+    if (parts.length === 2) {
+      return `${parts[1]} - ${parts[0]}`;
+    }
+  }
+  return song.title;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  fetch('/songs')
+    .then(response => response.json())
+    .then(data => {
+      songs = data;
+      songs.forEach(directory => {
+        directory.songs.forEach(song => {
+          song.title = formatSongTitle(song);
+        });
+      });
+      renderSongs(songs);
+    });
+  AOS.init();
+});
+
+function renderFolderContent(songs) {
+  const songList = document.getElementById('songList');
+  songList.innerHTML = '';
+
+  songs.forEach(song => {
+    const listItem = document.createElement('li');
+
+    const selectBox = document.createElement('input');
+    selectBox.type = 'checkbox';
+    selectBox.className = 'song-checkbox';
+    selectBox.addEventListener('change', toggleDownloadButton);
+    listItem.appendChild(selectBox);
+
+    const songTitle = document.createElement('span');
+    songTitle.textContent = formatSongTitle(song);
+    listItem.appendChild(songTitle);
+
+    listItem.addEventListener('click', () => {
+      loadSong(song.file);
+    });
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = song.file;
+    downloadLink.download = song.title;
+    downloadLink.innerHTML = '<i class="fas fa-download"></i>';
+    downloadLink.addEventListener('click', (event) => {
+      event.stopPropagation();
+    });
+    listItem.appendChild(downloadLink);
+
+    songList.appendChild(listItem);
+  });
+}
